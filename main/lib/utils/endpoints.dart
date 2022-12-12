b2c21991fe92297f781a7ca12632a7419d9322e1
alessiomodonesi <alessiomodo2004@gmail.com>
2022-12-12 21:26:08 +0100

    Update endpoints.dart

--- main/lib/utils/endpoints.dart
+++ main/lib/utils/endpoints.dart
@@ -1,11 +1,10 @@
-//const getTagUrl = "http://10.0.2.2:8080/food-api/API/tag/getTag.php?tag_ID=";
-const getTagUrl =
-    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/getTag.php?tag_ID=";
+const getTagUrl = "http://10.0.2.2:8080/food-api/API/tag/getTag.php?tag_ID=";
+// const getTagUrl = "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/getTag.php?tag_ID=";
 
-//const getProductTagUrl = "http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 const getProductTagUrl =
-    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+    "http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+// const getProductTagUrl = "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 
-//const getProductUrl = "http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
 const getProductUrl =
-    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/product/getProduct.php?PRODUCT_ID=";
+    "http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
+// const getProductUrl = "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/product/getProduct.php?PRODUCT_ID=";
