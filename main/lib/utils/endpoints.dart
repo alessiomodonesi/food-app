7f97edc0196417adcf0c1009ebacf5d4213e9a25
alessiomodonesi <alessiomodo2004@gmail.com>
2022-12-12 19:04:06 +0100

    Update endpoints.dart

--- main/lib/utils/endpoints.dart
+++ main/lib/utils/endpoints.dart
@@ -1,7 +1,11 @@
-const getTagUrl = "http://10.0.2.2:8080/food-api/API/tag/getTag.php?tag_ID=";
+//const getTagUrl = "http://10.0.2.2:8080/food-api/API/tag/getTag.php?tag_ID=";
+const getTagUrl =
+    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/getTag.php?tag_ID=";
 
+//const getProductTagUrl = "http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 const getProductTagUrl =
-    "http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 
+//const getProductUrl = "http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
 const getProductUrl =
-    "http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
+    "https://raw.githubusercontent.com/alessiomodonesi/food-api/main/API/product/getProduct.php?PRODUCT_ID=";
