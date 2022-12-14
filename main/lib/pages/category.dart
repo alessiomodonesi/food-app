f914e29e22e3eeadd93c011d9a46b1c5604b884f
enricopaolotoso <74261261+enricopaolotoso@users.noreply.github.com>
2022-12-14 13:19:38 +0100

    bottombar category

--- main/lib/pages/category.dart
+++ main/lib/pages/category.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/pages/homepage.dart';
 import 'package:vertical_card_pager/vertical_card_pager.dart';
@@ -57,50 +58,74 @@ class HomePage extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: VerticalCardPager(
-                    titles: titles, // required
-                    images: images, // required
-                    textStyle: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold), // optional
-                    onPageChanged: (page) {
-                      // optional
-                    },
-                    onSelectedItem: (index) {
-                      // optional
-                    },
-                    initialPage: 0, // optional
-                    align: ALIGN.CENTER // optional
-                    ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Home'),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profilo',
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: VerticalCardPager(
+                      titles: titles, // required
+                      images: images, // required
+                      textStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold), // optional
+                      onPageChanged: (page) {
+                        // optional
+                      },
+                      onSelectedItem: (index) {
+                        // optional
+                      },
+                      initialPage: 0, // optional
+                      align: ALIGN.CENTER // optional
+                      ),
+                ),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Carrello',
-          ),
-        ],
-      ),
-    );
+        ),
+        bottomNavigationBar: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+              child: BottomNavigationBar(
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                backgroundColor: const Color.fromRGBO(166, 4, 0, 1),
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        CupertinoIcons.person_fill,
+                        color: Colors.white,
+                      ),
+                      label: ''),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      CupertinoIcons.home,
+                      color: Colors.white,
+                    ),
+                    label: '',
+                  ),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        CupertinoIcons.shopping_cart,
+                        color: Colors.white,
+                      ),
+                      label: '')
+                ],
+              ),
+            )));
   }
 }
