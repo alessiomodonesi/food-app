3d0662f9a052f9336761eb09146f4cf620ef6674
Mattia-Zanini <mattzanini2@gmail.com>
2022-12-16 16:40:49 +0100

    Piccole sistemazioni e bottone per tornare alla pagina precendete

--- sandwech/lib/pages/product.dart
+++ sandwech/lib/pages/product.dart
@@ -51,6 +51,13 @@ class _ProductPageState extends State<ProductPage> {
     return Scaffold(
       body: Stack(children: <Widget>[
         Image.asset(paninoPath),
+        Positioned(
+          top: 40,
+          left: 10,
+          child: ButtonCircle(30, () {
+            Navigator.pop(context);
+          }, Icons.arrow_back_ios),
+        ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
@@ -65,19 +72,20 @@ class _ProductPageState extends State<ProductPage> {
             width: MediaQuery.of(context).size.width,
             child: Container(
                 margin: const EdgeInsets.only(
-                    top: 10, bottom: 27, left: 27, right: 27),
+                    top: 10, bottom: 27, left: 27, right: 10),
                 child: Column(
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(
-                            top: 45,
-                          ),
-                          child: Align(
+                    SizedBox(
+                      height: calcPercentage(
+                          MediaQuery.of(context).size.height, 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // nome prodotto
+                          Align(
                             alignment: Alignment.bottomLeft,
                             child: Text(
-                              prodotto.name, // nome prodotto
+                              prodotto.name,
                               textAlign: TextAlign.left,
                               textScaleFactor: 1.9,
                               style: const TextStyle(
@@ -86,71 +94,117 @@ class _ProductPageState extends State<ProductPage> {
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: calcPercentage(
-                              MediaQuery.of(context).size.width, 12),
-                        ),
-                        SvgPicture.asset(
-                          "lib/assets/left_side_pills.svg",
-                          semanticsLabel: 'Pills',
-                          width: 50,
-                        ),
-                      ],
-                    ),
-                    const Divider(
-                      color: Colors.black,
-                      height: 20,
-                    ),
-                    const Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Text(
-                        "Descrizione",
-                        textAlign: TextAlign.left,
-                        textScaleFactor: 1.5,
-                        style: TextStyle(
-                          color: Colors.amber,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        prodotto.description as String,
-                        textScaleFactor: 1,
-                        style: const TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: const [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Text(
-                            "Ingredienti",
-                            textAlign: TextAlign.left,
-                            textScaleFactor: 1.5,
-                            style: TextStyle(
-                              color: Colors.amber,
-                              fontWeight: FontWeight.bold,
+                          //icona a destra del nome del prodotto
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: SvgPicture.asset(
+                              "lib/assets/left_side_pills.svg",
+                              semanticsLabel: 'Pills',
+                              width: 55,
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+
+                    //informazioni relative al prodotto, con tanto di pulsanti
+                    Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          const Divider(
+                            color: Colors.grey,
+                            height: 20,
+                          ),
+                          const Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              "Descrizione",
+                              textAlign: TextAlign.left,
+                              textScaleFactor: 1.5,
+                              style: TextStyle(
+                                color: Colors.amber,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              prodotto.description as String,
+                              textScaleFactor: 1,
+                              style: const TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(
+                                  "Ingredienti",
+                                  textAlign: TextAlign.left,
+                                  textScaleFactor: 1.5,
+                                  style: TextStyle(
+                                    color: Colors.amber,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     )
                   ],
                 )),
           ),
         )
       ]),
+    );
+  }
+}
+
+/// Widget per crare un pulsante rotondo, bianco
+class ButtonCircle extends StatelessWidget {
+  final double size;
+  final GestureTapCallback onTap;
+  final IconData iconData;
+
+  /// [double] size: rappresenta la grandezza del cerchio
+  ///
+  /// [GestureTapCallback] onTap: è la funzione che viene eseguita quando si clicca il pulsante
+  ///
+  /// [IconData] iconData: è l'icona che sta al centro del cerchio
+  const ButtonCircle(this.size, this.onTap, this.iconData, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkResponse(
+      onTap: onTap,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Icon(
+            iconData,
+            color: Colors.black,
+            size: 22,
+          ),
+        ),
+      ),
     );
   }
 }
