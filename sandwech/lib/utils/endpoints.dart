14903ffa7fc46cfa4bd8ab5c092d84129a9c93e9
AlbertoPavarin <albertopavarin@libero.it>
2022-12-16 19:13:22 +0100

    Passaggio da catalogo a singolo prodotto

--- sandwech/lib/utils/endpoints.dart
+++ sandwech/lib/utils/endpoints.dart
@@ -9,6 +9,9 @@ const getProductUrl =
     "http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
 // const getProductUrl = "http://localhost/food-api/API/product/getProduct.php?PRODUCT_ID=";
 
+//const getArchieveIngredientsUrl =
+//  "http://10.0.2.2:8080/food-api/API/product/getArchiveIngredients.php?panino=";
+
 // non togliere questi commenti
 //const getTagUrl = "http://IP_VM:8081/API/tag/getTag.php?tag_ID=";
 //const getProductTagUrl = "http://IP_VM:8081/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
