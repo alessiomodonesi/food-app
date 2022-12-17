6a4aba8102d78ad38b71a381b10ebd0a41d4696b
alessiomodonesi <alessiomodo2004@gmail.com>
2022-12-17 14:28:44 +0100

    ordinati endpoint

--- sandwech/lib/utils/endpoints.dart
+++ sandwech/lib/utils/endpoints.dart
@@ -1,20 +1,20 @@
+//Android
 //const getTagUrl = "http://10.0.2.2:8080/food-api/API/tag/getTag.php?tag_ID=";
-// const getTagUrl = "http://localhost/food-api/API/tag/getTag.php?tag_ID=";
-
-//const getProductTagUrl ="http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
-// const getProductTagUrl = "http://localhost/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
-
 //const getProductUrl ="http://10.0.2.2:8080/food-api/API/product/getProduct.php?PRODUCT_ID=";
-// const getProductUrl = "http://localhost/food-api/API/product/getProduct.php?PRODUCT_ID=";
+//const getProductTagUrl ="http://10.0.2.2:8080/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+//const getArchieveIngredientsUrl = "http://10.0.2.2:8080/food-api/API/product/getArchiveIngredients.php?panino=";
 
-//const getArchieveIngredientsUrl =
-//  "http://10.0.2.2:8080/food-api/API/product/getArchiveIngredients.php?panino=";
-
-// non togliere questi commenti
-const getTagUrl = "http://192.168.19.212:8081/API/tag/getTag.php?tag_ID=";
-const getProductTagUrl =
-    "http://192.168.19.212:8081/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+//IOS
+const getTagUrl = "http://localhost/food-api/API/tag/getTag.php?tag_ID=";
 const getProductUrl =
-    "http://192.168.19.212:8081/API/product/getProduct.php?PRODUCT_ID=";
+    "http://localhost/food-api/API/product/getProduct.php?PRODUCT_ID=";
+const getProductTagUrl =
+    "http://localhost/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 const getArchieveIngredientsUrl =
-    "http://192.168.19.212:8081/API/product/getArchiveIngredients.php?panino=";
+    "http://localhost/food-api/API/product/getArchiveIngredients.php?panino=";
+
+//Altro
+// const getTagUrl = "http://192.168.19.212:8081/API/tag/getTag.php?tag_ID=";
+// const getProductUrl = "http://192.168.19.212:8081/API/product/getProduct.php?PRODUCT_ID=";
+// const getProductTagUrl = "http://192.168.19.212:8081/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+// const getArchieveIngredientsUrl = "http://192.168.19.212:8081/API/product/getArchiveIngredients.php?panino=";
