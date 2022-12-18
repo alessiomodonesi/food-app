3c39160c334c56d6f54b38ac1b4c9164acb0a534
AlbertoPavarin <albertopavarin@libero.it>
2022-12-18 02:18:13 +0100

    Aggiunto controllo se un tag contiene dei prodotti in catalog

--- sandwech/lib/pages/catalog.dart
+++ sandwech/lib/pages/catalog.dart
@@ -166,123 +166,150 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ))),
             Container(
-              padding: const EdgeInsets.only(
-                top: 150,
-                left: 14,
-                right: 14,
-              ),
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: _productList.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onTap: () {
-                          log(_productList[index].id);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ProductPage(
-                                      int.parse(_productList[index].id))));
-                        },
-                        child: Container(
-                            padding: const EdgeInsets.only(top: 15),
-                            child: Material(
-                              color: Colors.white,
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width - 28,
-                                    height: 67,
-                                    child: DecoratedBox(
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                          color: const Color(0xEEEEEAEA),
-                                          boxShadow: const [
-                                            BoxShadow(
-                                                color:
-                                                    Color.fromARGB(64, 0, 0, 0),
-                                                blurRadius: 7,
-                                                offset: Offset(0, 4)),
-                                          ]),
-                                      child: Stack(
-                                        children: [
-                                          Container(
-                                            height: 55,
-                                            width: 55,
-                                            margin: const EdgeInsets.only(
-                                              left: 14,
-                                              top: 6,
-                                            ),
-                                            decoration: const BoxDecoration(
-                                              color: Color(0xEEEEEAEA),
-                                            ),
-                                            child: Image.asset(
-                                                'lib/assets/sandwich.png'),
-                                          ),
-                                          Container(
-                                              height: 20,
-                                              margin: const EdgeInsets.only(
-                                                top: 24,
-                                                left: 80,
-                                              ),
-                                              child: RichText(
-                                                text: TextSpan(
-                                                  text:
-                                                      _productList[index].name,
-                                                  style: const TextStyle(
-                                                      color: Colors.black,
-                                                      fontWeight:
-                                                          FontWeight.w700,
-                                                      fontStyle:
-                                                          FontStyle.normal,
-                                                      fontSize: 16,
-                                                      fontFamily: 'Inter'),
+                padding: const EdgeInsets.only(
+                  top: 150,
+                  left: 14,
+                  right: 14,
+                ),
+                child: (() {
+                  if (_productList.isEmpty) {
+                    return Center(
+                        child: RichText(
+                      text: const TextSpan(
+                        text: ("Nessun prodotto"),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16),
+                      ),
+                    ));
+                  } else {
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _productList.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                              onTap: () {
+                                log(_productList[index].id);
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => ProductPage(
+                                            int.parse(
+                                                _productList[index].id))));
+                              },
+                              child: Container(
+                                  padding: const EdgeInsets.only(top: 15),
+                                  child: Material(
+                                    color: Colors.white,
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width -
+                                              28,
+                                          height: 67,
+                                          child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(15),
+                                                color: const Color(0xEEEEEAEA),
+                                                boxShadow: const [
+                                                  BoxShadow(
+                                                      color: Color.fromARGB(
+                                                          64, 0, 0, 0),
+                                                      blurRadius: 7,
+                                                      offset: Offset(0, 4)),
+                                                ]),
+                                            child: Stack(
+                                              children: [
+                                                Container(
+                                                  height: 55,
+                                                  width: 55,
+                                                  margin: const EdgeInsets.only(
+                                                    left: 14,
+                                                    top: 6,
+                                                  ),
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                    color: Color(0xEEEEEAEA),
+                                                  ),
+                                                  child: Image.asset(
+                                                      'lib/assets/sandwich.png'),
                                                 ),
-                                              )),
-                                          Container(
-                                            height: 37,
-                                            width: 57,
-                                            margin: const EdgeInsets.only(
-                                              top: 15,
-                                              left: 302,
+                                                Container(
+                                                    height: 20,
+                                                    margin:
+                                                        const EdgeInsets.only(
+                                                      top: 24,
+                                                      left: 80,
+                                                    ),
+                                                    child: RichText(
+                                                      text: TextSpan(
+                                                        text:
+                                                            _productList[index]
+                                                                .name,
+                                                        style: const TextStyle(
+                                                            color: Colors.black,
+                                                            fontWeight:
+                                                                FontWeight.w700,
+                                                            fontStyle: FontStyle
+                                                                .normal,
+                                                            fontSize: 16,
+                                                            fontFamily:
+                                                                'Inter'),
+                                                      ),
+                                                    )),
+                                                Container(
+                                                  height: 37,
+                                                  width: 57,
+                                                  margin: const EdgeInsets.only(
+                                                    top: 15,
+                                                    left: 302,
+                                                  ),
+                                                  child: SizedBox(
+                                                      height: 37,
+                                                      width: 57,
+                                                      child: DecoratedBox(
+                                                        decoration: BoxDecoration(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        15),
+                                                            color: const Color(
+                                                                0xFFEE0F38)),
+                                                        child: Center(
+                                                            child: RichText(
+                                                                text: TextSpan(
+                                                          text:
+                                                              "\$  ${_productList[index].price}",
+                                                          style: const TextStyle(
+                                                              color:
+                                                                  Colors.white,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w700,
+                                                              fontStyle:
+                                                                  FontStyle
+                                                                      .normal,
+                                                              fontSize: 16,
+                                                              fontFamily:
+                                                                  'Inter'),
+                                                        ))),
+                                                      )),
+                                                ),
+                                              ],
                                             ),
-                                            child: SizedBox(
-                                                height: 37,
-                                                width: 57,
-                                                child: DecoratedBox(
-                                                  decoration: BoxDecoration(
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              15),
-                                                      color: const Color(
-                                                          0xFFEE0F38)),
-                                                  child: Center(
-                                                      child: RichText(
-                                                          text: TextSpan(
-                                                    text:
-                                                        "\$  ${_productList[index].price}",
-                                                    style: const TextStyle(
-                                                        color: Colors.white,
-                                                        fontWeight:
-                                                            FontWeight.w700,
-                                                        fontStyle:
-                                                            FontStyle.normal,
-                                                        fontSize: 16,
-                                                        fontFamily: 'Inter'),
-                                                  ))),
-                                                )),
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                ],
-                              ),
-                            )));
-                  }),
-            ),
+                                  )));
+                        });
+                  }
+                }()) /**/
+                ),
           ],
         ),
         bottomNavigationBar: const Navbar());
