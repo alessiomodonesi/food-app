938c6ea1aa509ec657c01ebff1d8979e17581564
Mattia-Zanini <mattzanini2@gmail.com>
2022-12-20 17:14:52 +0100

    riadattato endpoints

--- sandwech/lib/utils/endpoints.dart
+++ sandwech/lib/utils/endpoints.dart
@@ -3,15 +3,16 @@ const android = "10.0.2.2:8080";
 const ios = "localhost";
 const vm = ":8081";
 
+const baseUrl = "$ios/food-api";
+
 // url
-const getTagUrl = "http://$ios/food-api/API/tag/getTag.php?tag_ID=";
-const getProductUrl =
-    "http://$ios/food-api/API/product/getProduct.php?PRODUCT_ID=";
+const getTagUrl = "http://$baseUrl/API/tag/getTag.php?tag_ID=";
+const getProductUrl = "http://$baseUrl/API/product/getProduct.php?PRODUCT_ID=";
 const getProductTagUrl =
-    "http://$ios/food-api/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
+    "http://$baseUrl/API/tag/product-tag/getArchiveProductTag.php?tag_id=";
 const getArchieveIngredientsUrl =
-    "http://$ios/food-api/API/product/getArchiveIngredients.php?panino=";
-const postSignUpUrl = "http://$ios/food-api/API/user/registration.php";
-const addItemCartUrl = "http://$ios/food-api/API/cart/setAddItem.php";
-const postSignInUrl = "http://$ios/food-api/API/user/login.php";
-const getCartUrl = "http://$ios/food-api/API/cart/getCart.php?user=";
+    "http://$baseUrl/API/product/getArchiveIngredients.php?panino=";
+const postSignUpUrl = "http://$baseUrl/API/user/registration.php";
+const addItemCartUrl = "http://$baseUrl/API/cart/setAddItem.php";
+const postSignInUrl = "http://$baseUrl/API/user/login.php";
+const getCartUrl = "http://$baseUrl/API/cart/getCart.php?user=";
