553b232c2cf2a760f05de0bc55fda7fbb0e64391
alessiomodonesi <alessiomodo2004@gmail.com>
2022-12-21 08:34:45 +0100

    Create catalog_card.dart

---
+++ sandwech/lib/utils/catalog_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'package:sandwech/utils/size.dart';
+
+class CartCard extends StatefulWidget {
+  final int idCategory;
+  final String productName;
+  final String price;
+
+  const CartCard(this.idCategory, this.productName, this.price, {super.key});
+
+  @override
+  State<CartCard> createState() => _CartCardState();
+}
+
+class _CartCardState extends State<CartCard> {
+  String tagName(int id) {
+    switch (id) {
+      case 1:
+        return "panini";
+      case 2:
+        return "bibite";
+      case 3:
+        return "piadine";
+      case 4:
+        return "brioches";
+      case 5:
+        return "snack";
+    }
+    return "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        // ignore: prefer_const_constructors
+        elevation: 10,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        margin: const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+        color: const Color.fromARGB(255, 236, 235, 235),
+        child: Padding(
+            padding: const EdgeInsets.only(top: 0, bottom: 0),
+            child: ListTile(
+              leading: Image.asset(
+                'lib/assets/icons/${tagName(widget.idCategory)}-icon.png',
+                width: 60,
+                height: 60,
+              ),
+              title: Text(widget.productName,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: getFontSize(20))),
+              subtitle: Text(
+                "€ ${widget.price}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: getFontSize(18)),
+              ),
+              isThreeLine: false,
+            )));
+  }
+}
